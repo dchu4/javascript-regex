86c101b05964916d0911f1230afbf2cefedd4910
Daniel Chu <dchu@lyonscg.com>
2017-06-23 13:28:18 -0500

    displays No results when there is nothing

--- regular_expression_tester.js
+++ regular_expression_tester.js
@@ -3,18 +3,17 @@ var checkRegEx = function(){
 	var textContent = document.getElementById('text-content').value;
 
 	if(textContent && regexString){
+		//Check if re is valid
 		try{
 
-
 			var re = new RegExp(regexString, 'g');
-
-			console.log(re);
 
 			if(re){
 
 				var holder = [];
 				var xArray = []; 
 
+				//Loop through the RegEx matches
 				do{
 					holder = re.exec(textContent);
 					if(holder){
@@ -34,25 +33,37 @@ var checkRegEx = function(){
 		catch(err){
 			document.getElementById('match-results').innerHTML = "Regular Expression Invalid";
 		}
+	} else {
+		document.getElementById('match-results').innerHTML = "No results";
 	};
 
 };
 
+//Highlights all the matches
 var highlightAllText = function(matchArray, textContent, startIndex){
-	var popArray;
-	textOutput = "";
+	var shiftArray;
+	var textOutput = "";
 
 	if(matchArray.length == 1){
-		popArray = matchArray.shift();
+		shiftArray = matchArray.shift();
 
-		textOutput = textContent.substring(startIndex, popArray["index"]) + "<span style='background-color:yellow;'>" + textContent.substring(popArray["index"], popArray["index"] + popArray["match"].length) + "</span>" + textContent.substring(popArray["index"] + popArray["match"].length);
+		textOutput = highlight(shiftArray, textContent, startIndex) + textContent.substring(shiftArray["index"] + shiftArray["match"].length);
 
 		return textOutput;
 	}
 
-	popArray = matchArray.shift();
+	shiftArray = matchArray.shift();
 
-	textOutput = textContent.substring(startIndex, popArray["index"]) + "<span style='background-color:yellow;'>" + textContent.substring(popArray["index"], popArray["index"] + popArray["match"].length) + "</span>" + highlightAllText(matchArray, textContent, popArray["index"] + popArray["match"].length);
+	textOutput = highlight(shiftArray, textContent, startIndex) + highlightAllText(matchArray, textContent, shiftArray["index"] + shiftArray["match"].length);
 
 	return textOutput; 
+}
+
+//Highlights the match
+var highlight = function(match, textContent, startIndex){
+	var textOutput = "";
+
+	textOutput = textContent.substring(startIndex, match["index"]) + "<span style='background-color:yellow;'>" + textContent.substring(match["index"], match["index"] + match["match"].length) + "</span>";
+
+	return textOutput;
 }
